9951dd4e66d340f4d7d112a55d8cc282cdf67d35
Maxim Khitrov <max@mxcrypt.com>
2018-05-25 17:44:03 -0400

    Update region API

--- region/region.go
+++ region/region.go
@@ -1,55 +1,89 @@
-package scan
+package region
 
 import (
 	"sort"
+	"sync"
 
 	"github.com/aws/aws-sdk-go-v2/aws/endpoints"
 )
 
-// Maps of valid regions for each partition and service. These include global
-// regions, such as "aws-global" (see endpoints/defaults.go).
 var (
-	partRegions = make(map[string]map[string]struct{})
-	svcRegions  = make(map[string]map[string]struct{})
-	allRegions  = make(map[string]struct{})
+	once        sync.Once
+	regionPart  map[string]string
+	partRegions map[string][]string
+	svcRegions  map[string][]string
 )
 
-func init() {
-	for _, p := range endpoints.DefaultPartitions() {
-		pr := make(map[string]struct{})
-		partRegions[p.ID()] = pr
+// Partition returns the partition of the specified region.
+func Partition(region string) string {
+	once.Do(load)
+	return regionPart[region]
+}
+
+// Related returns all regions in the same partition as region.
+func Related(region string) []string {
+	once.Do(load)
+	if rel := partRegions[regionPart[region]]; len(rel) > 0 {
+		cpy := make([]string, len(rel))
+		copy(cpy, rel)
+		return cpy
+	}
+	return nil
+}
+
+// Supports returns true if service is supported in region. Matching is strict,
+// so services like IAM are considered to be only in the aws-global region.
+func Supports(region, service string) bool {
+	once.Do(load)
+	return contains(svcRegions[service], region) ||
+		(service == endpoints.Ec2metadataServiceID && region == "aws-global")
+}
+
+// load creates partition/region/service maps.
+func load() {
+	parts := endpoints.DefaultPartitions()
+	regionPart = make(map[string]string)
+	partRegions = make(map[string][]string, len(parts))
+	svcRegions = make(map[string][]string)
+	for _, p := range parts {
+		regionSet := make(map[string]struct{})
+		// Using Endpoints() instead of Regions() to handle global services
 		for _, s := range p.Services() {
-			sr := svcRegions[s.ID()]
-			if sr == nil {
-				sr = make(map[string]struct{})
-				svcRegions[s.ID()] = sr
+			if s.ID() == endpoints.Ec2metadataServiceID {
+				continue
 			}
-			for region := range s.Endpoints() {
-				// TODO: Local DynamoDB scanning requires timeouts, etc.
-				if region != "local" {
-					sr[region] = struct{}{}
-					pr[region] = struct{}{}
-					allRegions[region] = struct{}{}
+			srs := svcRegions[s.ID()]
+			eps := s.Endpoints()
+			tmp := make([]string, len(srs), len(srs)+len(eps))
+			copy(tmp, srs)
+			srs = tmp
+			for r := range eps {
+				if r != "local" {
+					srs = append(srs, r)
+					regionSet[r] = struct{}{}
 				}
 			}
+			svcRegions[s.ID()] = srs
 		}
+		pid := p.ID()
+		prs := make([]string, 0, len(regionSet))
+		for r := range regionSet {
+			if _, dup := regionPart[r]; dup {
+				panic("region: duplicate name: " + r)
+			}
+			regionPart[r] = pid
+			prs = append(prs, r)
+		}
+		sort.Strings(prs)
+		partRegions[pid] = prs
+	}
+	for _, sr := range svcRegions {
+		sort.Strings(sr)
 	}
 }
 
-// RelatedRegions returns all regions in the same partition as region.
-func RelatedRegions(region string) []string {
-	if region == "" {
-		region = endpoints.UsEast1RegionID
-	}
-	for _, r := range partRegions {
-		if _, ok := r[region]; ok {
-			all := make([]string, 0, len(r))
-			for name := range r {
-				all = append(all, name)
-			}
-			sort.Strings(all)
-			return all
-		}
-	}
-	panic("scan: invalid region: " + region)
+// contains returns true if v contains s.
+func contains(v []string, s string) bool {
+	i := sort.SearchStrings(v, s)
+	return i < len(v) && v[i] == s
 }
---
+++ region/region_test.go
@@ -0,0 +1,28 @@
+package region
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPartition(t *testing.T) {
+	assert.Equal(t, "aws", Partition("us-east-1"))
+	assert.Equal(t, "aws", Partition("aws-global"))
+	assert.Equal(t, "aws-us-gov", Partition("aws-us-gov-global"))
+}
+
+func TestRelated(t *testing.T) {
+	r := Related("us-east-1")
+	assert.Contains(t, r, "us-east-1")
+	assert.Contains(t, r, "us-west-1")
+	assert.NotContains(t, r, "us-gov-west-1")
+	assert.Nil(t, Related(""))
+}
+
+func TestSupports(t *testing.T) {
+	assert.True(t, Supports("us-east-1", "ec2"))
+	assert.False(t, Supports("us-east-1", "iam"))
+	assert.True(t, Supports("aws-global", "iam"))
+	assert.True(t, Supports("aws-global", "ec2metadata"))
+}
