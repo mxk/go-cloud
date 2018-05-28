1d62aa7d2bf96d01f0d1402558521b2589d4a0ed
Maxim Khitrov <max@mxcrypt.com>
2018-05-27 21:13:23 -0400

    Add awsmock package

---
+++ awsmock/config.go
@@ -0,0 +1,44 @@
+package awsmock
+
+import (
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/aws/defaults"
+	"github.com/aws/aws-sdk-go-v2/aws/endpoints"
+)
+
+// Config returns a config that calls fn instead of defaults.SendHandler. The
+// mock handler should set request's Data and/or Error fields as needed to
+// simulate AWS response.
+func Config(fn func(*aws.Request)) aws.Config {
+	cfg := aws.Config{
+		Credentials:      aws.AnonymousCredentials,
+		EndpointResolver: endpoints.NewDefaultResolver(),
+		Handlers:         defaults.Handlers(),
+		Retryer:          aws.DefaultRetryer{},
+		Logger:           defaults.Logger(),
+	}
+
+	// Remove/disable all data-related handlers
+	cfg.Handlers.Send.Remove(defaults.SendHandler)
+	cfg.Handlers.Send.Remove(defaults.ValidateReqSigHandler)
+	cfg.Handlers.ValidateResponse.Remove(defaults.ValidateResponseHandler)
+	disableHandlerList("awsmock.Unmarshal", &cfg.Handlers.Unmarshal)
+	disableHandlerList("awsmock.UnmarshalMeta", &cfg.Handlers.UnmarshalMeta)
+	//disableHandlerList("awsmock.UnmarshalError", &cfg.Handlers.UnmarshalError)
+
+	// Install mock handler
+	cfg.Handlers.Send.PushBackNamed(aws.NamedHandler{
+		Name: "awsmock.SendHandler",
+		Fn: func(req *aws.Request) {
+			req.Retryable = aws.Bool(false)
+			fn(req)
+		},
+	})
+	return cfg
+}
+
+// disableHandlerList prevents a HandlerList from executing any handlers.
+func disableHandlerList(name string, l *aws.HandlerList) {
+	l.PushFrontNamed(aws.NamedHandler{Name: name, Fn: func(*aws.Request) {}})
+	l.AfterEachFn = func(aws.HandlerListRunItem) bool { return false }
+}
---
+++ awsmock/config_test.go
@@ -0,0 +1,24 @@
+package awsmock
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
+	"github.com/stretchr/testify/require"
+)
+
+func TestConfig(t *testing.T) {
+	wantOut := new(sts.GetCallerIdentityOutput)
+	cfg := Config(func(r *aws.Request) { r.Data = wantOut })
+	out, err := sts.New(cfg).GetCallerIdentityRequest(nil).Send()
+	require.True(t, out == wantOut)
+	require.NoError(t, err)
+
+	wantErr := errors.New("error")
+	cfg = Config(func(r *aws.Request) { r.Error = wantErr })
+	out, err = sts.New(cfg).GetCallerIdentityRequest(nil).Send()
+	require.Nil(t, out)
+	require.True(t, err == wantErr)
+}
